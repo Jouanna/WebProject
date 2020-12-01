db9bee9abd7958f210f4a946cfce7ee2237624ef
User <User@DESKTOP-MS6O653>
2020-12-02 00:26:55 +0200

    models

--- WebProject2020/Data/Models/VehicleDetails.cs
+++ WebProject2020/Data/Models/VehicleDetails.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
+using WebProject2020.Models;
 
 namespace WebProject2020.Data.Models
 {
@@ -10,10 +12,16 @@ namespace WebProject2020.Data.Models
     {
         [Key]
         public int Id { get; set; }
+
         [Required]
+        [ForeignKey("Details")]
         public int DetailsId { get; set; }
+        public Details Detail { get; set; }
+
         [Required]
+        [ForeignKey("Vehicle")]
         public int VehicleId { get; set; }
+        public Vehicle Vehicle { get; set; }
 
     }
 }
